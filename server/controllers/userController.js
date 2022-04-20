6af5a93916b4b634615110d55af6ba64468a7d8e
Daniel Bae <danibae91@icloud.com>
2022-04-19 23:09:11 -0700

    implement jwt, bcrypt and protected routes for user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,14 @@
+const jwt = require("jsonwebtoken"); 
+const bcrypt = require("bcryptjs"); 
 const asyncHandler = require("express-async-handler"); 
 const User = require("../models/userModel"); 
+
+// Generate JWT (token)
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: "30d"
+    });
+};
 
 // @description: Register New User 
 // @route: POST /api/users
@@ -18,11 +27,18 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error("User already exists");
     };
 
-    // create user with username, email, password
+    // hash password with BCRYPTJS
+    // - generate salt with 10 rounds (max 12) 
+    const salt = await bcrypt.genSalt(10); 
+    // - hash password
+    const hashedPassword = await bcrypt.hash(password, salt); 
+
+    // create user with username, email, password 
+    // - add hashedPassword 
     const user = await User.create({
         username, 
         email, 
-        password
+        password: hashedPassword
     }); 
 
     if(user) {
@@ -31,6 +47,7 @@ const registerUser = asyncHandler(async (req, res) => {
             username: user.username, 
             email: user.email, 
             createdAt: user.createdAt, 
+            token: generateToken(user._id) // provides token for AUTHORIZATION
         })
     } else {
         res.status(400)
@@ -48,12 +65,13 @@ const signInUser = asyncHandler(async (req, res) => {
     const user = await User.findOne({ email });
 
     // if this is the user
-    if(user) {
+    if(user && (await bcrypt.compare(password, user.password))) {
         res.json({
             _id: user.id, 
             username: user.username, 
             email: user.email, 
             createdAt: user.createdAt, 
+            token: generateToken(user._id) // provides token for AUTHORIZATION
         });
     } else {
         res.status(400) 
@@ -68,15 +86,16 @@ const getUser = asyncHandler(async(req, res) => {
     const user = await User.findById(req.params.id)
     if(!user) {
         res.status(400); 
-        throw new Error('User not found'); 
+        throw new Error("User not found"); 
     }
     await User.findById(req.params.id)
     res.status(200).json({ 
         _id: user.id, 
-        userusername: user.userusername, 
+        username: user.username, 
         email: user.email, 
         isAdmin: user.isAdmin, 
         createdAt: user.createdAt, 
+        token: generateToken(user._id) // provides token for AUTHORIZATION
     }); 
 });
 
@@ -87,13 +106,17 @@ const updateUser = asyncHandler(async (req, res) => {
     const user = await User.findById(req.params.id); 
     if(!user) {
         res.status(400); 
-        throw new Error('User not found'); 
+        throw new Error("User not found"); 
     };
 
-    // UPDATE ONLY USERusername/EMAIL
-    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    // allow any change to be updated 
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    // re-hash user's password and save it to database
+    updatedUser.password = await bcrypt.hash(updatedUser.password, 10); 
+    updatedUser.save(); 
+
     res.status(200).json(updatedUser); 
-})
+});
 
 // @description: Delete User 
 // @route: DELETE /api/users/:id
@@ -102,11 +125,11 @@ const deleteUser = asyncHandler(async(req, res) => {
     const user = await User.findById(req.params.id)
     if(!user) {
         res.status(400); 
-        throw new Error('User not found'); 
-    }
-    await User.findByIdAndDelete(req.params.id)
+        throw new Error("User not found"); 
+    };
+    await User.findByIdAndDelete(req.params.id);
     res.status(200).json({ id: req.params.id }); 
-})
+});
 
 module.exports = {
     registerUser, 
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,11 +1,12 @@
 const express = require("express"); 
 const router = express.Router(); 
 const { registerUser, signInUser, getUser, updateUser, deleteUser } = require("../controllers/userController"); 
+const { protect } = require("../middleware/authMiddleware"); 
 
 router.post("/", registerUser); 
 router.post("/sign_in", signInUser); 
-router.get("/:id", getUser); 
-router.put("/:id", updateUser); 
-router.delete("/:id", deleteUser); 
+router.get("/:id", protect, getUser); 
+router.put("/:id", protect, updateUser); 
+router.delete("/:id", protect, deleteUser); 
 
 module.exports = router
