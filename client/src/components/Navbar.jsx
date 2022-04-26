bb750c83a0c580e7ad0173e679e1fbd83248fcd6
Daniel Bae <danibae91@icloud.com>
2022-04-26 15:51:50 -0700

    work on mobile responsiveness on components

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react"; 
 import { Link, useNavigate } from "react-router-dom"; 
 import { useSelector, useDispatch } from "react-redux"; 
 import { logout, reset } from "../redux/auth/authSlice"; 
 import { linkItems } from "../constants/data";
+import { motion } from "framer-motion"; 
 import { GiBoba } from "react-icons/gi"; 
+import { BiDotsVerticalRounded, BiArrowToRight } from "react-icons/bi"; 
 import "./Navbar.scss"; 
 
 const Navbar = () => {
+    // useState 
+    const [toggle, setToggle] = useState(false); 
+
     // useNavigate
     const navigate = useNavigate(); 
 
@@ -58,6 +64,40 @@ const Navbar = () => {
                         </>
                     )}
                     </ul>
+                </div>
+                <div className="nav-mobile">
+                    <BiDotsVerticalRounded onClick={() => setToggle(true)} />
+                        {toggle && (
+                            <motion.div
+                                whileInView={{ x: [100, 0] }}
+                                transition={{ duration: 0.45, ease: 'easeOut' }}
+                            >
+                                <BiArrowToRight onClick={() => setToggle(false)} />
+                                    <ul>
+                                    {linkItems.map((item) => (
+                                        <li key={item.id} >
+                                            <a href={item.url} onClick={() => setToggle(false)} >
+                                                {item.text}
+                                            </a>
+                                        </li>
+                                    ))}
+                                    { user ? (
+                                        <button onClick={handleLogout}>
+                                            Logout
+                                        </button>
+                                    ) : (
+                                        <>
+                                        <li>
+                                            <Link to="/login" onClick={() => setToggle(false)} >Login</Link>
+                                        </li>
+                                        <li>
+                                            <Link to="/register" onClick={() => setToggle(false)} >Register</Link>
+                                        </li>
+                                        </>
+                                    )}
+                                    </ul>
+                            </motion.div>
+                        )}
                 </div>
             </div>
         </div>
