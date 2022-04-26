34c43d0276300bcb081eb62e44bb2b0d43897293
Daniel Bae <danibae91@icloud.com>
2022-04-26 11:27:32 -0700

    retry changes for deployment

--- client/src/redux/auth/authService.js
+++ client/src/redux/auth/authService.js
@@ -1,12 +1,20 @@
 /* AuthService will handle HTTP requests with axios */ 
 import axios from "axios"; 
 // remember proxy: localhost:4000 (backend) 
-const API_URL = "/api/users"; 
+// const API_URL = "/api/users/"; 
+
+// Deployment 
+const API_URL = process.env.REACT_APP_API_URL; 
+const REGISTER_URL = `${API_URL}/api/users`; 
+const LOGIN_URL = `${API_URL}/api/users/sign_in`;
 
 // Register user
 const register = async (userData) => {
     // make a POST request and pass in userData to URL 
-    const response = await axios.post(API_URL, userData); 
+    // const response = await axios.post(API_URL, userData); 
+
+    // use process.env (deployment)
+    const response = await axios.post(REGISTER_URL, userData); 
     if(response.data) {
         // save user (token) into localStorage as string
         localStorage.setItem("user", JSON.stringify(response.data)); 
@@ -16,10 +24,15 @@ const register = async (userData) => {
 
 // Login user
 const login = async (userData) => {
-    const response = await axios.post(API_URL + '/sign_in', userData);
+    // const response = await axios.post(API_URL + 'sign_in', userData);
+    
+    // use process.env (deployment)
+    const response = await axios.post(LOGIN_URL, userData);
+  
     if (response.data) {
       localStorage.setItem('user', JSON.stringify(response.data));
     }; 
+  
     return response.data;
 };
 
