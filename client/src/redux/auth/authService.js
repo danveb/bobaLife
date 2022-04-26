e6237f24d8a6613def7c64bc9694f191d59eea5b
Daniel Bae <danibae91@icloud.com>
2022-04-25 18:29:19 -0700

    update proxy for deployment

--- client/src/redux/auth/authService.js
+++ client/src/redux/auth/authService.js
@@ -1,15 +1,12 @@
 /* AuthService will handle HTTP requests with axios */ 
 import axios from "axios"; 
 // remember proxy: localhost:4000 (backend) 
-// const API_URL = "/api/users/"; 
+const API_URL = "/api/users"; 
 
 // Register user
 const register = async (userData) => {
     // make a POST request and pass in userData to URL 
-    // const response = await axios.post(API_URL, userData); 
-    
-    // use process.env (deployment)
-    const response = await axios.post(process.env.REACT_APP_API_URL, userData); 
+    const response = await axios.post(API_URL, userData); 
     if(response.data) {
         // save user (token) into localStorage as string
         localStorage.setItem("user", JSON.stringify(response.data)); 
@@ -19,15 +16,10 @@ const register = async (userData) => {
 
 // Login user
 const login = async (userData) => {
-    // const response = await axios.post(API_URL + 'sign_in', userData);
-    
-    // use process.env (deployment)
-    const response = await axios.post(process.env.REACT_APP_API_URL + '/sign_in', userData);
-  
+    const response = await axios.post(API_URL + '/sign_in', userData);
     if (response.data) {
       localStorage.setItem('user', JSON.stringify(response.data));
     }; 
-  
     return response.data;
 };
 
