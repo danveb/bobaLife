c7c10a8cb300deeb45aa54a8913fe1fe608f479c
Daniel Bae <danibae91@icloud.com>
2022-09-05 21:44:03 -0700

    checkout helper API address update for deployment

--- client/src/helper/checkoutHelper.js
+++ client/src/helper/checkoutHelper.js
@@ -1,7 +1,7 @@
 // Checkout API for backend
 // for API we use backend URL
 // for deployment use main URL we deploy server to
-const API = "http://localhost:4000"
+const API = process.env.REACT_APP_API_URL
 
 export async function fetchFromAPI(endpoint, options) {
     const { method, body } = { method: "POST", body: null, ...options }; 
